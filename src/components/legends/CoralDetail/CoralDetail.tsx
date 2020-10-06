92237186f5e9a85ecc93acfabe7bb68f47c707be
cursedseraphim <cat-noir@gmx.at>
2020-10-06 09:33:54 +0200

    coral fingerprint style as scss

--- src/components/legends/CoralDetail/CoralDetail.tsx
+++ src/components/legends/CoralDetail/CoralDetail.tsx
@@ -12,6 +12,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import './coral.scss';
 
 const useStyles = makeStyles({
   table: {
