e2049553a2ba14474ffeddae3047a013fe899467
cursedseraphim <cat-noir@gmx.at>
2020-11-01 09:53:29 +0100

    remove zoom

--- src/components/legends/CoralDetail/VegaDate.js
+++ src/components/legends/CoralDetail/VegaDate.js
@@ -4,13 +4,6 @@ import {createClassFromLiteSpec} from 'react-vega-lite';
 export default createClassFromLiteSpec('VegaDate', {
   "width": 50,
   "height": 50,
-  "selection": {
-    "brush": {
-      "type": "interval",
-      "encodings": ["x"],
-      "bind": "scales"
-    }
-  },
   "mark": {"type": "bar", "tooltip": true},
   "transform": [{"filter": "datum.feature != null"}],
   "encoding": {
@@ -18,10 +11,7 @@ export default createClassFromLiteSpec('VegaDate', {
       "field": "feature",
       "type": "temporal",
       "bin": {
-        "maxbins": 10,
-        "extent": {
-          "selection": "brush"
-        }
+        "maxbins": 10
       },
       "axis": null
     },
--- src/components/legends/CoralDetail/VegaHist.js
+++ src/components/legends/CoralDetail/VegaHist.js
@@ -4,22 +4,12 @@ import {createClassFromLiteSpec} from 'react-vega-lite';
 export default createClassFromLiteSpec('VegaHist', {
   "width": 50,
   "height": 50,
-  "selection": {
-    "brush": {
-      "type": "interval",
-      "encodings": ["x"],
-      "bind": "scales"
-    }
-  },
   "mark": {"type": "bar", "tooltip": true},
   "transform": [{"filter": "datum.feature != null"}],
   "encoding": {
     "x": {
       "bin": {
-        "maxbins": 10,
-        "extent": {
-          "selection": "brush"
-        }
+        "maxbins": 10
       },
       "field": "feature",
       "axis": null
