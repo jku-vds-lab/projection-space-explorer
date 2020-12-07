32e8b43f8b5c9bf5b0f61744d96fcf2219d8d43d
Christina <ginihumer96@yahoo.de>
2020-12-07 17:12:00 +0100

    backend

---
+++ src/components/legends/ChemDetail/ChemDetail.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react'
+import './chem.scss'
+import * as backend_utils from '../../../utils/backend-connect'
+
+/**
+ * Chem Legend, implemented
+ */
+
+
+export class ChemLegend extends React.Component{
+
+    constructor(props: { selection, aggregate, columns }){
+        super(props);
+        this.state = {
+            comp: <div>no SMILES column found</div>,
+            current_selection: null
+        };
+
+    }
+
+    loadImage(){
+        if(this.state.current_selection == this.props.selection){
+
+        }else{
+            let smiles_col = "SMILES";
+
+            for (const col_name in this.props.columns) {
+                let col = this.props.columns[col_name];
+                if(col.meta_data && col.meta_data.includes("smiles_to_img"))
+                    smiles_col = col_name;
+            }
+            if(smiles_col in this.props.columns){
+                if (this.props.aggregate) {
+                    const formData = new FormData();
+                    this.props.selection.forEach(row => {
+                        formData.append('smiles_list', row[smiles_col]);
+                    });
+                    backend_utils.get_structures_from_smiles_list(formData).then(x => {
+                        const img_lst = x["img_lst"].map((base64,i) => <img key={formData.getAll("smiles_list")[i]} className={"legend_multiple"} src={"data:image/gif;base64," + base64}/>)
+                        this.setState({
+                            comp: img_lst, 
+                            current_selection:this.props.selection
+                        })
+                    });
+                }else{
+                    let row = this.props.selection[0];
+                    backend_utils.get_structure_from_smiles(row[smiles_col]).then(x => this.setState({comp: <img className={"legend_single"} src={"data:image/gif;base64," + x}/>, current_selection:this.props.selection}));
+                }
+            }else{
+                this.setState({comp: <div>no SMILES column found</div>, current_selection:this.props.selection})
+            }
+        }
+    }
+
+    render(){
+        this.loadImage();
+        return this.state.comp;
+    }
+}
---
+++ src/utils/backend-connect.ts
@@ -0,0 +1,57 @@
+
+const BASE_URL = 'http://127.0.0.1:8080';
+
+export async function get_structure_from_smiles(smiles:string) {
+    const formData = new FormData();
+    formData.append('smiles', smiles);
+    return fetch(BASE_URL+'/get_mol_img', {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.text())
+    .catch(error => {
+        console.error(error)
+    });
+}
+
+export async function get_structures_from_smiles_list(formData:FormData){
+    return fetch(BASE_URL+'/get_mol_imgs', {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.json())
+    .catch(error => {
+        console.error(error)
+    });
+}
+
+
+export async function get_mcs_from_smiles_list(formData:FormData) {
+    return fetch(BASE_URL+'/get_common_mol_img', {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.text())
+    .catch(error => {
+        console.error(error)
+    });
+    
+}
+
+
+export async function upload_sdf_file(file){
+    // upload the sdf file to the server
+    // the response is a unique filename that can be used to make further requests
+    const formData_file = new FormData();
+    formData_file.append('myFile', file);
+
+    return fetch(BASE_URL+'/upload_sdf', {
+        method: 'POST',
+        body: formData_file,
+    })
+    .then(response => response.json())
+    .catch(error => {
+        console.error(error);
+    });
+}
+
