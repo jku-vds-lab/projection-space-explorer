49dcd0dd6d86498589fd0d8736793d5ea046c434
Christina <ginihumer96@yahoo.de>
2021-03-01 11:57:56 +0100

    chemvisV0.1.7

--- src/utils/frontend-connect.ts
+++ src/utils/frontend-connect.ts
@@ -1,7 +1,7 @@
 // CONSTANTS
 
-export const BASE_PATH = "dist/"; // use this when you want to use the index.html in the root folder
-// export const BASE_PATH = ""; //use this when you want to use the index.html in the dist folder --> for docker
+// export const BASE_PATH = "dist/"; // use this when you want to use the index.html in the root folder
+export const BASE_PATH = ""; //use this when you want to use the index.html in the dist folder --> for docker
 
 export const CHEM_PROJECT = true; // use this flag to show that it the tool is used for chem data
 // export const CHEM_PROJECT = false; // use this for the default PSE and for netlify
