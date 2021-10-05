4fe07b50f3a398d56b437dadd67474c4b0721338
Moritz Schöfl <moritz.schoefl@datavisyn.io>
2021-10-05 08:50:37 +0200

    bundle test

--- dist/exports.d.ts
+++ dist/exports.d.ts
@@ -1,5 +1,5 @@
 import { PSEContextProvider } from "./components/Store/PSEContext";
 import { Application } from ".";
-import { PluginRegistry } from "./components/Store/PluginScript";
+import { PluginRegistry, API } from "./components/Store/PluginScript";
 import { HoverTabPanel } from "./components/DrawerTabPanels/HoverTabPanel/HoverTabPanel";
-export { PSEContextProvider, Application, PluginRegistry, HoverTabPanel };
+export { PSEContextProvider, Application, PluginRegistry, HoverTabPanel, API };
