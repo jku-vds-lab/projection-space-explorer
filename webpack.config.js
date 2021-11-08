c28241a56b9402640b3cd3e4edeec319477f97f3
ginihumer <45741696+ginihumer@users.noreply.github.com>
2021-11-08 17:31:28 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "development",
   watch: true,
   entry: {
-    bundle: "./src/components/exports.tsx",
+    bundle: "./src/components/index.tsx",
     tsne: './src/components/workers/embeddings/worker_tsne.ts',
     umap: './src/components/workers/embeddings/worker_umap.ts',
     cluster: './src/components/workers/worker_cluster.tsx',
