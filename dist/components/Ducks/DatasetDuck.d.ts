79fc5d340444f56769b11db2cdaaa7cc3a96d25c
cursedseraphim <cat-noir@gmx.at>
2021-12-29 11:17:29 +0100

    add set fctn to datasetduck

--- dist/components/Ducks/DatasetDuck.d.ts
+++ dist/components/Ducks/DatasetDuck.d.ts
@@ -1,2 +1,28 @@
 import { Dataset } from "../../model/Dataset";
-export default function dataset(state?: Dataset): Dataset;
+export function dataset(state?: Dataset): Dataset;
+
+const SET = "ducks/dataset/SET"
+interface SetDatasetAction {
+    type: typeof SET
+    dataset: Dataset
+}
+
+type DatasetActionTypes = SetDatasetAction
+
+export function setDatasetAction(dataset: Dataset): DatasetActionTypes {
+    return {
+        type: SET,
+        dataset: dataset
+    }
+}
+
+const initialState: Dataset = null
+
+export default function Dataset(state = initialState, action): Dataset {
+    switch (action.type) {
+        case SET:
+            return action.dataset
+        default:
+            return state
+    }
+}
