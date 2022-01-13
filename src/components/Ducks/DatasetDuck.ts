f77a05015e7f68bb412ae5b6d145cef661021084
ginihumer <45741696+ginihumer@users.noreply.github.com>
2022-01-13 14:44:53 +0100

    Update DatasetDuck.ts

--- src/components/Ducks/DatasetDuck.ts
+++ src/components/Ducks/DatasetDuck.ts
@@ -1,28 +1,6 @@
 import { Dataset } from "../../model/Dataset";
-// export function dataset(state?: Dataset): Dataset;
-
-const SET = "ducks/dataset/SET"
-interface SetDatasetAction {
-    type: typeof SET
-    dataset: Dataset
-}
-
-type DatasetActionTypes = SetDatasetAction
-
-export function setDatasetAction(dataset: Dataset): DatasetActionTypes {
-    return {
-        type: SET,
-        dataset: dataset
-    }
-}
 
 const initialState: Dataset = null
-
-export default function setDataset(state = initialState, action): Dataset {
-    switch (action.type) {
-        case SET:
-            return action.dataset
-        default:
-            return state
-    }
+export default function dataset(state = initialState): Dataset {
+    return state
 }
