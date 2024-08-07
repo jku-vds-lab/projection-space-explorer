c3cf62cc163b6fa2991e0a51c489e10380cd290a
Moritz Heckmann <moritz.heckmann@datavisyn.io>
2024-08-07 10:05:19 +0200

    changed visualy to visually

--- src/components/DrawerTabPanels/DetailsTabPanel/DetailsTabPanel.tsx
+++ src/components/DrawerTabPanels/DetailsTabPanel/DetailsTabPanel.tsx
@@ -64,7 +64,7 @@ export const DetailsTabPanel = connector(
       <div key={dataset?.info?.path} style={{ display: 'flex', flexDirection: 'column', height: '100%', paddingBottom: 1 }}>
         <Box paddingX={2} paddingTop={2} paddingBottom={1}>
           <Typography variant="subtitle2" gutterBottom>
-            Visualy summarize {globalLabels.itemLabelPlural}
+            Visually summarize {globalLabels.itemLabelPlural}
           </Typography>
           <Typography variant="body2" color="textSecondary" gutterBottom>
             Interactively select {globalLabels.itemLabelPlural} to update the summary visualization.{' '}
